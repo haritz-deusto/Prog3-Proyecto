caf5899224dceead4a193a386f353585ebec9884
EnriqueCorres <enrique.corres@opendeusto.es>
2024-10-16 14:50:10 +0200

    Creacion de clases y maximización de ventanas

--- Prog3-Proyecto/src/main/VentanaArranque.java
+++ Prog3-Proyecto/src/main/VentanaArranque.java
@@ -8,21 +8,32 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class VentanaPrincipal  extends JFrame{
+public class VentanaArranque  extends JFrame{
 	private static final long serialVersionUID = 1L;
 	public static void main(String[] args) {
         JFrame ventana = new JFrame("Deusto Stream");
         ventana.setSize(1000, 800);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLocationRelativeTo(null);
+        ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
         
         JPanel panelSuperior = new JPanel();
         panelSuperior.setLayout(new FlowLayout(FlowLayout.RIGHT));
         JButton botonInicioSesion = new JButton("Iniciar Sesión");
         JButton botonRegistro = new JButton("Registrase");
+        JButton botonPrueba = new JButton("Boton Prueba");
+        panelSuperior.add(botonPrueba);
         panelSuperior.add(botonInicioSesion);
         panelSuperior.add(botonRegistro);
         ventana.add(panelSuperior, BorderLayout.NORTH);
+        
+        botonPrueba.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+            	ventanaPrincipal.setVisible(true);
+			}
+		});
         
         botonRegistro.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -35,6 +46,7 @@ public class VentanaPrincipal  extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 VentanaInicioSesion ventanaInicioSesion = new VentanaInicioSesion();
                 ventanaInicioSesion.setVisible(true);
+            	
             }
         });
         
--- Prog3-Proyecto/src/main/VentanaInicioSesion.java
+++ Prog3-Proyecto/src/main/VentanaInicioSesion.java
@@ -9,5 +9,6 @@ class VentanaInicioSesion extends JFrame {
         setSize(1000, 800);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Para cerrar solo esta ventana
         setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 }
