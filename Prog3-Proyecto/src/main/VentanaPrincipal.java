0f7604cf7169405123ebdfbadbf6e2a76af8fd76
r.alonso <r.alonso@opendeusto.es>
2024-10-16 12:34:40 +0200

    ventana principal creada

--- Prog3-Proyecto/src/main/VentanaPrincipal.java
+++ Prog3-Proyecto/src/main/VentanaPrincipal.java
@@ -1,5 +1,13 @@
 package main;
 
-public class VentanaPrincipal {
+import javax.swing.JFrame;
 
+public class VentanaPrincipal {
+	public static void main(String[] args) {
+        JFrame ventana = new JFrame("Deusto Stream");
+        ventana.setSize(1000, 800);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
 }
