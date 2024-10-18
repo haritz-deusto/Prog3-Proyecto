49f198893fd7003ce10833ba50d8ed2f9c1ec3f2
EnriqueCorres <enrique.corres@opendeusto.es>
2024-10-18 18:25:18 +0200

    Botones de "Volver" en ventanas Inicio de sesion y registro

--- Prog3-Proyecto/src/gui/VentanaInicioSesion.java
+++ Prog3-Proyecto/src/gui/VentanaInicioSesion.java
@@ -29,7 +29,7 @@ class VentanaInicioSesion extends JFrame {
         setSize(1000, 800);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Para cerrar solo esta ventana
         setLocationRelativeTo(null);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         
         panelCentro = new JPanel();
         panelCentro.setLayout(new GridLayout(6,1));
@@ -63,5 +63,10 @@ class VentanaInicioSesion extends JFrame {
 		panelCentro.add(txtContrasenia);
 		
         panelSur.add(btnVolver);
+        
+        
+        btnVolver.addActionListener((e)->{
+        	dispose();
+        });
     }
 }
--- Prog3-Proyecto/src/gui/VentanaRegistro.java
+++ Prog3-Proyecto/src/gui/VentanaRegistro.java
@@ -1,15 +1,42 @@
 package gui;
 
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 class VentanaRegistro extends JFrame{
+	  
+	private JPanel panelCentro, panelSur, panelNorte, panelEste, panelOeste;
+	    
+	private JButton btnVolver;
 	public VentanaRegistro() {
 	        setTitle("Registro");
 	        setSize(1000, 800);
 	        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Para cerrar solo esta ventana
 	        setLocationRelativeTo(null);
-	        setExtendedState(JFrame.MAXIMIZED_BOTH);
+	        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	        
+	     panelCentro = new JPanel();
+	     panelSur = new JPanel();
+	     btnVolver = new JButton("Volver");
+	     
+	     
+	     
+	     
+	     
+	     getContentPane().add(panelCentro, BorderLayout.CENTER);
+	     getContentPane().add(panelSur, BorderLayout.SOUTH);
+	     
+	     
+	     panelSur.add(btnVolver);
+	     
+	     
+	     btnVolver.addActionListener((e)->{
+	    	 dispose();
+	     });
 	}
 }
