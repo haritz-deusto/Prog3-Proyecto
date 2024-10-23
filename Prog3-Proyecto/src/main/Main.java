af6a18d752ff223780774ab86ece817c6cbd2dcb
haritz-deusto <haritz.carvalho@opendeusto.es>
2024-10-23 10:11:31 +0200

    ultima prueba

--- Prog3-Proyecto/src/main/Main.java
+++ Prog3-Proyecto/src/main/Main.java
@@ -5,6 +5,5 @@ import gui.VentanaArranque;
 public class Main {
 	public static void main(String[] args) {
 		VentanaArranque ventanaArranque = new VentanaArranque();
-		System.out.println("test haritz 2");
 	}
 }
