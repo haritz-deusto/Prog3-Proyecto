e7149ce5b270fd469f7d9c5a6609746e787fa748
haritz-deusto <haritz.carvalho@opendeusto.es>
2024-10-28 10:52:33 +0100

    añadida clase comentario

---
+++ Prog3-Proyecto/src/domain/Comentario.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class Comentario extends Pelicula{
+	protected Usuario usuario;
+	protected String texto;
+	protected LocalDate fecha;
+
+	public Comentario(String titulo, int duracion, String descripcion, Genero genero, Valoracion estrellas,
+			Usuario usuario, String texto, LocalDate fecha) {
+		super(titulo, duracion, descripcion, genero, estrellas);
+		this.usuario = usuario;
+		this.texto = texto;
+		this.fecha = fecha;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	@Override
+	public String toString() {
+		return "Comentario [usuario=" + usuario + ", texto=" + texto + ", fecha=" + fecha + "]";
+	}
+}
